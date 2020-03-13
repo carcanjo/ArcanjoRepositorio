c81350b3ee1d7eb8fb4c2ad407a8f63411ecf1ae
carcanjo <cleiton.arcanjo@outlook.com>
2020-03-13 14:23:56 -0300

    Projeto final

---
+++ ProjetoArcanjo/FI.AtividadeEntrevista/BLL/BoCliente.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoCliente
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Incluir(cliente);
+        }
+
+        /// <summary>
+        /// Altera um cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public void Alterar(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Alterar(cliente);
+        }
+
+        /// <summary>
+        /// Consulta o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public DML.Cliente Consultar(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Listar()
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Listar();
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Pesquisa(iniciarEm,  quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// VerificaExistencia
+        /// </summary>
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string CPF)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.VerificarExistencia(CPF);
+        }
+
+        public bool ValidaCPF(string cpf)
+        {
+            int multiplicador1 = 10;
+            int multiplicador2 = 11;
+
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+
+            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+
+            if (cpf == "11111111111")
+                return false;
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * (multiplicador1 - i);
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * (multiplicador2 - i);
+
+            resto = soma % 11;
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+    }
+}
